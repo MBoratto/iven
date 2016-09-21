6c35e607f9df0268debe2e4fcea3765adb8731b1
Jeeh Campos <jessicacampos123@yahoo.com.br>
2016-09-21 07:08:39 +0000

    Updated routing protocol

--- Routing/routing.cpp
+++ Routing/routing.cpp
@@ -2,11 +2,11 @@
 #include <bcm2835.h>
 #include <stdio.h>
 #include <queue.h>
+#include <unordered_map.h>
 #include "../MRF24J/mrf24j.h"
 
 #define NUM_ATTEMPTS 3
-
-char message_number;
+#define MSG_LIFETIME 60
 
 typedef struct _message_list {
 	char * message;
@@ -21,10 +21,19 @@ typedef struct _message_lifetime {
 } message_lifetime;
 
 std::queue<message_list> message_queue;
+std::unordered_multimap<uint64_t, message_lifetime> message_map;
+
+char message_number;
+uint64_t self_address, dest_address, src_address;
 
 int handle_packets(void) {
 	char routing_control = (mrf.get_rxinfo()->rx_data[0] & 0xc0) >> 6;
 	message_number = mrf.get_rxinfo()->rx_data[0] & 0x3f;
+	
+	self_address = mrf.address64_read();
+	dest_address = mrf.get_dest_address64();
+	src_address = mrf.get_src_address64();
+	
 	switch (routing_control) {
 		case 0:
 			handle_message();
@@ -39,7 +48,7 @@ int handle_packets(void) {
 			handle_fowarding();
 			break;
 		case 4:
-			handle_ack();
+			handle_nack();
 			break;
 		case 5:
 			handle_scan();
@@ -48,7 +57,7 @@ int handle_packets(void) {
 }
 
 int handle_message(void) {
-	if(mrf.address64_read() == mrf.get_dest_address64()) {
+	if(self_address == dest_address) {
 		
 	} else {
 		return 0;
@@ -56,17 +65,19 @@ int handle_message(void) {
 }
 
 int handle_routing(void) {
-	if(mrf.address64_read() == mrf.get_dest_address64()) {
+	if(self_address == dest_address) {
 		// handle message and return ack
+		send_ack();
 	} else {
 		if(new_message()) {
 			message_list tmp_list;
 			tmp_list.message = mrf.get_rxinfo()->rx_data;
-			tmp_list.address = mrf.get_dest_address64();
+			tmp_list.address = dest_address;
 			tmp_list.number = message_number ;
 			tmp_list.attempts = NUM_ATTEMPTS;
 			message_queue.push(tmp_list);
-			// return node ack
+			
+			send_nack(src_address);// return node ack
 		} else {
 			// return flooding control
 			return 0;
@@ -75,11 +86,23 @@ int handle_routing(void) {
 }
 
 bool new_message() {
-	
+	auto range = message_map.equal_range(dest_address);
+	if(range.first != range.second) {
+		for(auto it = range.first; it != range.second; it++) {
+			if(it->second.number == message_number) {
+				return false;
+			} 
+		}
+	}
+	message_lifetime tmp_message;
+	tmp_message.number = message_number;
+	tmp_message.lifetime = MSG_LIFETIME;
+	message_map.insert(std::make_pair<uint64_t, message_lifetime>(dest_address, tmp_message));
+	return true;
 }
 
 int handle_flooding(void) {
-	uint64_t dest_addr = mrf.get_dest_address64();
+	uint64_t dest_addr = routed_dest_address64(); // correct!
 	std::queue<message_list> tmp_queue;
 	while(!message_queue.empty()) {
 		message_list tmp_list = message_queue.front();
@@ -99,8 +122,9 @@ int handle_flooding(void) {
 	}
 }
 
-int handle_fowarding(void) {
-	uint64_t dest_addr = mrf.get_dest_address64();
+int handle_nack(void) {
+	//check if is to me
+	uint64_t dest_addr = routed_dest_address64(); // correct!
 	std::queue<message_list> tmp_queue;
 	while(!message_queue.empty()) {
 		message_list tmp_list = message_queue.front();
