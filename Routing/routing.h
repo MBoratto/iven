6816ab62e5a412461a5a09ea0966469d92390dac
Jeeh Campos <jessicacampos123@yahoo.com.br>
2016-10-14 22:10:28 -0300

    Updated final ack on routing

--- Routing/routing.h
+++ Routing/routing.h
@@ -34,7 +34,7 @@ void handle_ack(Mrf24j& mrf);
 int handle_scan(void);
 void send_flood(Mrf24j& mrf, uint64_t src_addr, uint64_t msg_address);
 void send_nack(Mrf24j& mrf, uint64_t src_address, uint64_t msg_address);
-void send_ack(uint64_t dest_addr, uint64_t msg_address);
+void send_ack(Mrf24j& mrf, uint64_t dest_addr, uint64_t msg_address);
 std::queue<message_list> get_queue(void);
 uint64_t routed_dest_address64(void);
 
