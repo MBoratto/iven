7b1de87303e1b52fc154ddf546a743319d57896b
loop777 <ricardo_alcantara2@hotmail.com>
2016-10-24 14:24:58 -0200

    Create sinistronotifica.py
    Arquivo para notificação em celular

---
+++ Notifications/sinistronotifica.py
@@ -0,0 +1,57 @@
+import time
+import RPi.GPIO as GPIO
+import pycurl, json
+
+
+GPIO.setmode(GPIO.BCM)
+# tempo entre notificoes em segundo
+
+DELAY = 20
+
+# InstaPush API setup
+appID = "580b6afca4c48a4b693a9426"
+appSecret = "4934d1e32b7eff991c6b96b94a6aa1b3"
+pushEvent = "Acidente_"
+pushMessage = "Acidente a frente"
+
+
+# use Curl to post to the Instapush API
+c = pycurl.Curl()
+
+# set API URL
+c.setopt(c.URL, 'https://api.instapush.im/v1/post')
+
+#setup custom headers for authentication variables and content type
+c.setopt(c.HTTPHEADER, ['x-instapush-appid: ' + appID,
+			'x-instapush-appsecret: ' + appSecret,
+			'Content-Type: application/json'])
+
+
+# create a dict structure for the JSON data to post
+json_fields = {}
+
+# setup JSON values
+json_fields['event']=pushEvent
+json_fields['trackers'] = {}
+json_fields['trackers']['message']=pushMessage
+#print(json_fields)
+postfields = json.dumps(json_fields)
+
+# make sure to send the JSON with post
+c.setopt(c.POSTFIELDS, postfields)
+
+# set this so we can capture the resposne in our buffer
+#c.setopt(c.WRITEFUNCTION, buffer.write)
+
+# uncomment to see the post sent
+c.setopt(c.VERBOSE, True)
+
+try:
+	# Loop infinito que envia push notificantions a cada 20 segundos 
+	while True:
+		c.perform()
+		print(pushMessage)
+		# tempo de espera para envio de nova mensagem
+		time.sleep(DELAY)
+except KeyboardInterrupt:
+        GPIO.cleanup()
